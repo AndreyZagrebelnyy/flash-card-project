e3cdf7f074ee37f76642731a2538756d4b03f2ce
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2024-06-21 14:12:09 +0300

    feat Controller: done

--- controller/controller.js
+++ controller/controller.js
@@ -6,6 +6,21 @@ class Controller {
     const userCategory = await this.view.startQuiz();
 
     console.log(Вы выбрали категорию "${userCategory.category}");
-    
+    let arrOfQuestions
+
+    if(userCategory.category === "111111"){    //! _______----------Wait data
+        arrOfQuestions = await this.model.getQuestionsAndAnswers("111.txt"); //!----------Wait data
+
+    } 
+    if(userCategory.category === "222"){    //! _______----------Wait data
+        arrOfQuestions = await this.model.getQuestionsAndAnswers("222.txt"); //!----------Wait data
+    }
+    if(userCategory.category === "333"){    //! _______----------Wait data
+        arrOfQuestions = await this.model.getQuestionsAndAnswers("333.txt"); //!----------Wait data
+    }
+    for (const question of arrOfQuestions) {
+        counter = await this.view.pullQuestions(question, counter);
+      }
+    console.log(`Поздравляем! Ты выиграл ${counter} очков в категории: ${userCategory.category}`) 
   }
 }
